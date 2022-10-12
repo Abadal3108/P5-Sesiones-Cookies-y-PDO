86ec7355fdb696ba74e509e1143be62df24412c4
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-10-12 20:17:55 +0200

    EX_03 mitad

--- EX_03/index.php
+++ EX_03/index.php
@@ -1,1 +1,20 @@
-<?php
+<html>
+<body>
+<form action="cookies.php" method="POST" name="formulario">
+    <input type="text" name="nombre">
+
+    <input type="submit" value="Enviar">
+    <?php
+    if (isset($_COOKIE['nombre'])) {
+        echo "hola qie lo q";
+        $borrarname = "";
+        setcookie('puntos', $_COOKIE['puntos'] = 0, time() + 3600
+        );
+        setcookie('nombre', $borrarname , time() + 3600
+        );
+    }
+    ?>
+</form>
+</body>
+</html>
+
